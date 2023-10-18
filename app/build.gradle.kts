8d1ee11aebd99d020a4ea88b2ce36cf89c47d8fd
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-18 15:28:08 +0700

    [INTERNET] Web Services and Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
   debugImplementation("androidx.compose.ui:ui-test-manifest")
   debugImplementation("androidx.compose.ui:ui-tooling")
 }
