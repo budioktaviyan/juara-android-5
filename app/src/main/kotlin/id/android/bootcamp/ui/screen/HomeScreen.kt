188f32fd8d5dde510ca5c9a78afa471aca078dab
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-10-18 15:59:56 +0700

    [INTERNET] Add Internet Permission and Exception Handling

--- app/src/main/kotlin/id/android/bootcamp/ui/screen/HomeScreen.kt
+++ app/src/main/kotlin/id/android/bootcamp/ui/screen/HomeScreen.kt
@@ -1,12 +1,18 @@
 package id.android.bootcamp.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,12 +21,25 @@ import id.android.bootcamp.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
-  marsUiState: String,
+  marsUiState: MarsUiState,
   modifier: Modifier = Modifier,
-  contentPadding: PaddingValues = PaddingValues(0.dp), ) {
-  ResultScreen(
-    marsUiState,
-    modifier.padding(top = contentPadding.calculateTopPadding())
+  contentPadding: PaddingValues = PaddingValues(0.dp)) {
+  when (marsUiState) {
+    is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+    is MarsUiState.Success -> ResultScreen(
+      marsUiState.photos,
+      modifier = modifier.padding(top = contentPadding.calculateTopPadding())
+    )
+    is MarsUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
+  }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+  Image(
+    modifier = modifier.size(200.dp),
+    painter = painterResource(R.drawable.loading_img),
+    contentDescription = stringResource(R.string.loading)
   )
 }
 
@@ -33,8 +52,27 @@ fun ResultScreen(
   modifier: Modifier = Modifier) {
   Box(
     contentAlignment = Alignment.Center,
-    modifier = modifier) {
+    modifier = modifier
+  ) {
     Text(text = photos)
+  }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.ic_connection_error),
+      contentDescription = ""
+    )
+    Text(
+      text = stringResource(R.string.loading_failed),
+      modifier = Modifier.padding(16.dp)
+    )
   }
 }
 
